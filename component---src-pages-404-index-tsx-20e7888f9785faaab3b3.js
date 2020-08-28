2e36c948546cddc35c1a8fc3a590206cca975ba1
Travis CI User <travis@example.org>
2020-08-28 11:58:38 +0000

    Updates

---
+++ component---src-pages-404-index-tsx-20e7888f9785faaab3b3.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{GEoZ:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),u=n("Ac47"),l=n("PIHY");t.default=function(){return a.createElement(u.a,null,a.createElement(l.a,{title:"404: Not found"}),a.createElement("h1",null,"NOT FOUND"),a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-index-tsx-20e7888f9785faaab3b3.js.map
