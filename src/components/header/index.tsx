b90102f7f07915fcac7442057a0208ea56eb70e6
Goran Peshanski <gpeshans@hfour.com>
2020-09-08 14:35:55 +0200

    initial logo

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,9 +1,37 @@
 import * as React from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
+import Img, { FluidObject } from 'gatsby-image';
 
 import './index.scss';
 
-interface HeaderProps {
-  siteTitle?: string;
+interface ImgData {
+  logo: {
+    childImageSharp: {
+      fluid: FluidObject;
+    };
+  };
 }
 
-export const Header = ({ siteTitle = '' }: HeaderProps) => <header className="aci-Header">{siteTitle}</header>;
+export const Header = () => {
+  const data = useStaticQuery<ImgData>(graphql`
+    query {
+      logo: file(relativePath: { eq: "logo/artcraftink-icon.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 100, maxHeight: 100) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `);
+
+  return (
+    <header className="aci-Header">
+      <Img
+        fluid={data.logo.childImageSharp.fluid}
+        imgStyle={{ objectFit: 'contain', objectPosition: 'center top' }}
+        fadeIn={true}
+      />
+    </header>
+  );
+};
