08aba5100914c5fcdd1d95b8630895c2eb7f3f35
Goran Peshanski <gpeshans@hfour.com>
2020-09-12 22:55:15 +0200

    extract faq and optimized banner

---
+++ src/data/faq.ts
@@ -0,0 +1,44 @@
+interface Faq {
+  question: string;
+  answer: string;
+}
+
+export const faq: Faq[] = [
+  {
+    question: 'How do i look after my tattoo?',
+    answer:
+      'No need to worry, we will go through everything with you while in the shop. We have a n aftercare sheet we will supply as well as plastic wrap, tape and aftercare ointment (which we offer for a little extra kuai). You are in good hands, we will not leave you hanging dry.',
+  },
+  {
+    question: 'Can you remove it for me?',
+    answer:
+      'Sorry but no! We can offer to cover your tattoo up with something you’ll love or give it a good ol’ refreshment. Our highly experienced artists here at Shanghai Tattoo are masters of cover ups and that tattoo you once regretted may end up being the tattoo you always wanted and now cant stop looking at because you love so much.',
+  },
+  {
+    question: 'Do tattoos hurt?',
+    answer: 'Yes!',
+  },
+  {
+    question: "Can i get a tattoo if i'm under 18?",
+    answer: 'No, Be patient. Tattoos are forever so wait till you are of age.',
+  },
+  {
+    question: 'How should I prepare for my tattoo session?',
+    answer:
+      'Make sure you are well rested, well hydrated, and have eaten recently. Aside from that, take a shower, charge your phone or tablet up to full, and don’t bring in your 50 closest friends (1-2 are fine).',
+  },
+  {
+    question: 'When should I not get tattooed?',
+    answer:
+      'You should not get tattooed when you are: Drunk, High, Pregnant, Pre-Surgery, Post-Surgery, When you don’t know what you want, When you can’t afford it, When you’re sunburned, When you haven’t showered, When you have your kids with you, If you don’t have an ID, Being pressured into it, When you’ve been told by us that your idea is bad',
+  },
+  {
+    question: 'Is tanning OK when you have tattoos?',
+    answer:
+      'It might be really tempting to get some rays during the summer months or hit up the tanning salon, but before you ruin your tattoo, please read about tattoos and tanning.',
+  },
+  {
+    question: 'What brand of ink do you use?',
+    answer: 'If you have allergies to specific ingredients or materials, now is the time to bring them up.',
+  },
+];
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
 import { Row, Column } from '../components/grid';
 import { Page } from '../components/page';
 import { Button } from '../components/button';
+import { faq } from '../data/faq';
 
 import './index.scss';
 
@@ -50,54 +51,12 @@ const IndexPage = () => {
               <Column className="aci-Section__faq-title" spanSm={12} spanMd={12} spanLg={12} spanXl={12}>
                 FAQ
               </Column>
-              <Column className="aci-Section__faq-content" spanSm={12} spanMd={6} spanLg={6} spanXl={6}>
-                <div className="aci-Section__faq-content-question">How do i look after my tattoo?</div>
-                <div className="aci-Section__faq-content-answer">
-                  No need to worry, we will go through everything with you while in the shop. We have a n aftercare
-                  sheet we will supply as well as plastic wrap, tape and aftercare ointment (which we offer for a little
-                  extra kuai). You are in good hands, we will not leave you hanging dry.
-                </div>
-              </Column>
-              <Column className="aci-Section__faq-content" spanSm={12} spanMd={6} spanLg={6} spanXl={6}>
-                <div className="aci-Section__faq-content-question">Can you remove it for me?</div>
-                <div className="aci-Section__faq-content-answer">
-                  Sorry but no! We can offer to cover your tattoo up with something you’ll love or give it a good ol’
-                  refreshment. Our highly experienced artists here at Shanghai Tattoo are masters of cover ups and that
-                  tattoo you once regretted may end up being the tattoo you always wanted and now cant stop looking at
-                  because you love so much.
-                </div>
-              </Column>
-              <Column className="aci-Section__faq-content" spanSm={12} spanMd={6} spanLg={6} spanXl={6}>
-                <div className="aci-Section__faq-content-question">Do tattoos hurt?</div>
-                <div className="aci-Section__faq-content-answer">Yes!</div>
-              </Column>
-              <Column className="aci-Section__faq-content" spanSm={12} spanMd={6} spanLg={6} spanXl={6}>
-                <div className="aci-Section__faq-content-question">Can i get a tattoo if i'm under 18?</div>
-                <div className="aci-Section__faq-content-answer">
-                  No, Be patient. Tattoos are forever so wait till you are of age.
-                </div>
-              </Column>
-              <Column className="aci-Section__faq-content" spanSm={12} spanMd={6} spanLg={6} spanXl={6}>
-                <div className="aci-Section__faq-content-question">Can you remove it for me?</div>
-                <div className="aci-Section__faq-content-answer">
-                  Sorry but no! We can offer to cover your tattoo up with something you’ll love or give it a good ol’
-                  refreshment. Our highly experienced artists here at Shanghai Tattoo are masters of cover ups and that
-                  tattoo you once regretted may end up being the tattoo you always wanted and now cant stop looking at
-                  because you love so much.
-                </div>
-              </Column>
-              <Column className="aci-Section__faq-content" spanSm={12} spanMd={6} spanLg={6} spanXl={6}>
-                <div className="aci-Section__faq-content-question">How do i look after my tattoo?</div>
-                <div className="aci-Section__faq-content-answer">
-                  No need to worry, we will go through everything with you while in the shop. We have a n aftercare
-                  sheet we will supply as well as plastic wrap, tape and aftercare ointment (which we offer for a little
-                  extra kuai). You are in good hands, we will not leave you hanging dry.
-                </div>
-              </Column>
-              <Column className="aci-Section__faq-content" spanSm={12} spanMd={6} spanLg={6} spanXl={6}>
-                <div className="aci-Section__faq-content-question">Do tattoos hurt?</div>
-                <div className="aci-Section__faq-content-answer">Yes!</div>
-              </Column>
+              {faq.map(({ question, answer }) => (
+                <Column className="aci-Section__faq-content" spanSm={12} spanMd={6} spanLg={6} spanXl={6}>
+                  <div className="aci-Section__faq-content-question">{question}</div>
+                  <div className="aci-Section__faq-content-answer">{answer}</div>
+                </Column>
+              ))}
             </Row>
           </section>
 
