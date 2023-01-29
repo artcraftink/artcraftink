ce3e7aeebb061e39000fa8d8f4ae4e4c781ad93c
gpeshans <pesanski.goran@gmail.com>
2023-01-29 17:13:31 +0100

    upgraded eslint and typescript

--- src/components/page/index.tsx
+++ src/components/page/index.tsx
@@ -37,14 +37,14 @@ export class Page extends React.Component<PageProps> {
   // fix for non-constant height for 100vh on mobile devices
   componentDidMount() {
     // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-    let vh = window.innerHeight * 0.01;
+    const vh = window.innerHeight * 0.01;
     // Then we set the value in the --vh custom property to the root of the document
     document.documentElement.style.setProperty('--vh', `${vh}px`);
 
     // We listen to the resize event
     window.addEventListener('resize', () => {
       // We execute the same script as before
-      let vh = window.innerHeight * 0.01;
+      const vh = window.innerHeight * 0.01;
       document.documentElement.style.setProperty('--vh', `${vh}px`);
     });
 
